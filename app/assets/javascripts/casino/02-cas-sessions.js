b7aa01d4a869bc369c694f0367fc08e7af5c5130
hrabizadeh <hrabizadeh@gmail.com>
2014-07-12 21:04:35 +0430

    debug mobile menu

---
+++ app/assets/javascripts/casino/02-cas-sessions.js
@@ -0,0 +1,38 @@
+(function (win, doc) {
+    var url = '/login',
+        cookie_regex = /(^|;)\s*tgt=/,
+        ready_bound = false;
+
+    function checkCookieExists() {
+        var serviceEl = doc.getElementById('service'),
+            service = serviceEl ? serviceEl.getAttribute('value') : null;
+
+        if (cookie_regex.test(doc.cookie)) {
+            url = '/login';
+            if (service) {
+                url += '?service=' + encodeURIComponent(service);
+            }
+            win.location = url;
+        } else {
+            setTimeout(checkCookieExists, 1000);
+        }
+    }
+
+    // Auto-login when logged-in in other browser window (9887c4e)
+    doc.addEventListener('DOMContentLoaded', function () {
+        if (ready_bound) {
+            return;
+        }
+        ready_bound = true;
+        if (doc.getElementById('login-form')) {
+            checkCookieExists();
+        }
+    });
+
+})(this, document);
+
+
+$(function () {
+    $('#flash_error').addClass('alert alert-danger');
+    $('#logout').html('<span class="glyphicon glyphicon-off"></span>Logout')
+});
