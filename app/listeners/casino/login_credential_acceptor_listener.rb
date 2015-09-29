3a465b530a2ddb39aeb9653fbdbfd06837775e22
Mani Malekmohammadi <mm.mani@gmail.com>
2015-09-29 17:00:48 +0330

    fix cookies problem with domain change

--- app/listeners/casino/login_credential_acceptor_listener.rb
+++ app/listeners/casino/login_credential_acceptor_listener.rb
@@ -20,8 +20,8 @@ class CASino::LoginCredentialAcceptorListener < CASino::Listener
     acceptto = Acceptto::Client.new(Rails.configuration.mfa_app_uid, Rails.configuration.mfa_app_secret, '')
     @channel = acceptto.authenticate(ticket_granting_ticket.acceptto_authentication_token, I18n.t("acceptto_mfa_authenticator.wishing_to_authorize"), I18n.t("acceptto_mfa_authenticator.mfa_authetication_type"), {ip_address: @controller.request.ip, remote_ip_address: @controller.request.remote_ip})
     @controller.session[:channel] = @channel
-    callback_url = "#{Rails.configuration.mfa_site}/cass/mfa/check?tgt=#{ticket_granting_ticket.ticket}"
-    callback_url = "#{callback_url}&service=#{service}" if !service.blank?
+    callback_url = "#{request.protocol + request.host_with_port}/cass/mfa/check?tgt=#{ticket_granting_ticket.ticket}"
+    callback_url = "#{callback_url}&service=#{service}" unless service.blank?
     redirect_url = "#{Rails.configuration.mfa_site}/mfa/index?channel=#{@channel}&callback_url=#{callback_url}"
     p "**********************************************************************"
     p "acceptto_authentication_pending setting callback_url to #{callback_url}"
