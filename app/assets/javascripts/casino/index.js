dbd9f6ad3dd833ac188c0caf06f78a91a5451820
Mani Malek <mm.mani@gmail.com>
2018-12-04 16:54:46 -0800

    use bootstrap-sass

--- app/assets/javascripts/casino/index.js
+++ app/assets/javascripts/casino/index.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree .
-
+//= require jquery-min
+//= require bootstrap-sprockets
+//= require cas-sessions
 
